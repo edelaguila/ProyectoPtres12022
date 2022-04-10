5852b088f35960b6a129f1c6bde6ccdba7a577e3
DiegoCortezUMG <98113773+DiegoCortezUMG@users.noreply.github.com>
2022-04-09 22:19:11 -0600

    Agregado el cls, dao, y mantenimiento para conceptosn esperando revisión

--- ProyectoP312022/src/main/java/compras/vista/frmMantenimientoConcepto.java
+++ ProyectoP312022/src/main/java/compras/vista/frmMantenimientoConcepto.java
@@ -7,8 +7,8 @@ package compras.vista;
 
 import seguridad.vista.*;
 import compras.vista.*;
-import compras.modelo.daoProducto;
-import compras.controlador.clsProducto;
+import compras.modelo.daoConcepto;
+import compras.controlador.clsConcepto;
 
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
@@ -18,45 +18,43 @@ import java.io.File;
  *
  * @author visitante
  */
-public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
+public class frmMantenimientoConcepto extends javax.swing.JInternalFrame {
 
   
 
     public void llenadoDeTablas() {
         DefaultTableModel modelo = new DefaultTableModel();
-        modelo.addColumn("ID Proveedor");
-        modelo.addColumn("ID Producto");
-        modelo.addColumn("Nombre Producto");
-        modelo.addColumn("Marca Producto");
-        modelo.addColumn("Precio Producto");
-        daoProducto productoDAO = new daoProducto();
-        List<clsProducto> producto = productoDAO.select();
+        modelo.addColumn("ID Concepto");
+        modelo.addColumn("Nombre Concepto");
+        modelo.addColumn("Efecto Concepto");
+        modelo.addColumn("Estado Concepto");
+        daoConcepto conceptoDAO = new daoConcepto();
+        List<clsConcepto> concepto = conceptoDAO.select();
         tablaVendedores.setModel(modelo);
         String[] dato = new String[5];
-        for (int i = 0; i < producto.size(); i++) {
-            dato[0] = Integer.toString(producto.get(i).getProdid());
-            dato[1] = Integer.toString(producto.get(i).getProvid());
-            dato[2] = producto.get(i).getProdnombre();
-            dato[3] = producto.get(i).getProdmarca();
-            dato[4] = Integer.toString(producto.get(i).getProdprecio());
+        for (int i = 0; i < concepto.size(); i++) {
+            dato[0] = Integer.toString(concepto.get(i).getConid());
+            dato[2] = concepto.get(i).getConnombre();
+            dato[3] = concepto.get(i).getConefecto();
+            dato[4] = concepto.get(i).getConestatus();
             //System.out.println("vendedor:" + vendedores);
             modelo.addRow(dato);
         }
     }
 
-    public void buscarProducto() {
-        clsProducto productoAConsultar = new clsProducto();
-        daoProducto productoDAO = new daoProducto();
-        productoAConsultar.setProdid(Integer.parseInt(txtbuscado.getText()));
-        productoAConsultar = productoDAO.query(productoAConsultar);
-        txtProveedor.setText(String.valueOf(productoAConsultar.getProvid()));
-        txtNombre.setText(productoAConsultar.getProdnombre());
-        txtMarca.setText(productoAConsultar.getProdmarca());
-        txtPrecio.setText(String.valueOf(productoAConsultar.getProdprecio()));
+    public void buscarConcepto() {
+        clsConcepto conceptoAConsultar = new clsConcepto();
+        daoConcepto conceptoDAO = new daoConcepto();
+        conceptoAConsultar.setConid(Integer.parseInt(txtbuscado.getText()));
+        conceptoAConsultar = conceptoDAO.query(conceptoAConsultar);
+        txtConId.setText(String.valueOf(conceptoAConsultar.getConid()));
+        txtConNombre.setText(conceptoAConsultar.getConnombre());
+        txtConEfecto.setText(conceptoAConsultar.getConefecto());
+        txtConEstado.setText(conceptoAConsultar.getConestatus());
         
     }
 
-    public frmMantenimientoProducto() {
+    public frmMantenimientoConcepto() {
         initComponents();
         llenadoDeTablas();
        
@@ -80,18 +78,18 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
         btnModificar = new javax.swing.JButton();
         label3 = new javax.swing.JLabel();
         txtbuscado = new javax.swing.JTextField();
-        txtProveedor = new javax.swing.JTextField();
+        txtConId = new javax.swing.JTextField();
         btnLimpiar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaVendedores = new javax.swing.JTable();
-        txtPrecio = new javax.swing.JTextField();
+        txtConEstado = new javax.swing.JTextField();
         label5 = new javax.swing.JLabel();
         lb = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         label6 = new javax.swing.JLabel();
         label7 = new javax.swing.JLabel();
-        txtNombre = new javax.swing.JTextField();
-        txtMarca = new javax.swing.JTextField();
+        txtConNombre = new javax.swing.JTextField();
+        txtConEfecto = new javax.swing.JTextField();
         label4 = new javax.swing.JLabel();
 
         lb2.setForeground(new java.awt.Color(204, 204, 204));
@@ -101,7 +99,7 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
         setIconifiable(true);
         setMaximizable(true);
         setResizable(true);
-        setTitle("Mantenimiento Productos");
+        setTitle("Mantenimiento Conceptos");
         setVisible(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -146,9 +144,9 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
         getContentPane().add(label3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 330, -1, -1));
         getContentPane().add(txtbuscado, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 360, 102, -1));
 
-        txtProveedor.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        txtProveedor.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        getContentPane().add(txtProveedor, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 220, 20));
+        txtConId.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtConId.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
+        getContentPane().add(txtConId, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 220, 20));
 
         btnLimpiar.setText("Limpiar");
         btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
@@ -179,12 +177,12 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
 
         getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(409, 20, 612, 303));
 
-        txtPrecio.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        txtPrecio.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        getContentPane().add(txtPrecio, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 220, -1));
+        txtConEstado.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtConEstado.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
+        getContentPane().add(txtConEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 220, -1));
 
         label5.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label5.setText("Precio Producto");
+        label5.setText("Estado Concepto");
         getContentPane().add(label5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, -1, -1));
 
         lb.setForeground(new java.awt.Color(204, 204, 204));
@@ -200,23 +198,23 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 300, 100, -1));
 
         label6.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label6.setText("Nombre Producto");
+        label6.setText("Nombre Concepto");
         getContentPane().add(label6, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 100, -1, -1));
 
         label7.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label7.setText("Marca Producto");
+        label7.setText("Efecto Concepto");
         getContentPane().add(label7, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, -1, -1));
 
-        txtNombre.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        txtNombre.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        getContentPane().add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 220, -1));
+        txtConNombre.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtConNombre.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
+        getContentPane().add(txtConNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 220, -1));
 
-        txtMarca.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        txtMarca.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        getContentPane().add(txtMarca, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 220, -1));
+        txtConEfecto.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtConEfecto.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
+        getContentPane().add(txtConEfecto, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 220, -1));
 
         label4.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label4.setText("Codigo Proveedor");
+        label4.setText("Id Concepto");
         getContentPane().add(label4, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 65, -1, -1));
 
         pack();
@@ -224,50 +222,48 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
 
     private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
         // TODO add your handling code here:
-        daoProducto productoDAO = new daoProducto();
-        clsProducto productoAEliminar = new clsProducto();
-        productoAEliminar.setProdid(Integer.parseInt(txtbuscado.getText()));
-        productoAEliminar.setProvid(Integer.parseInt(txtProveedor.getText()));
-        productoDAO.delete(productoAEliminar);
+        daoConcepto conceptoDAO = new daoConcepto();
+        clsConcepto conceptoAEliminar = new clsConcepto();
+        conceptoAEliminar.setConid(Integer.parseInt(txtbuscado.getText()));
+        conceptoDAO.delete(conceptoAEliminar);
         llenadoDeTablas();
     }//GEN-LAST:event_btnEliminarActionPerformed
 
     private void btnRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarActionPerformed
-        daoProducto productoDAO = new daoProducto();
-        clsProducto productoAInsertar = new clsProducto();
-        productoAInsertar.setProvid(Integer.parseInt(txtProveedor.getText()));
-        productoAInsertar.setProdnombre(txtNombre.getText());
-        productoAInsertar.setProdmarca(txtMarca.getText());
-        productoAInsertar.setProdprecio(Integer.parseInt(txtPrecio.getText()));
-        productoDAO.insert(productoAInsertar);
+        daoConcepto conceptoDAO = new daoConcepto();
+        clsConcepto conceptoAInsertar = new clsConcepto();
+        conceptoAInsertar.setConid(Integer.parseInt(txtConId.getText()));
+        conceptoAInsertar.setConnombre(txtConNombre.getText());
+        conceptoAInsertar.setConefecto(txtConEfecto.getText());
+        conceptoAInsertar.setConestatus(txtConEstado.getText());
+        conceptoDAO.insert(conceptoAInsertar);
         llenadoDeTablas();
     }//GEN-LAST:event_btnRegistrarActionPerformed
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         // TODO add your handling code here:
-        buscarProducto();
+        buscarConcepto();
     }//GEN-LAST:event_btnBuscarActionPerformed
 
     private void btnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
 //        // TODO add your handling code here:
-        daoProducto productoDAO = new daoProducto();
-        clsProducto productoAActualizar = new clsProducto();
-        productoAActualizar.setProdid(Integer.parseInt(txtbuscado.getText()));
-        productoAActualizar.setProvid(Integer.parseInt(txtProveedor.getText()));
-        productoAActualizar.setProdnombre(txtNombre.getText());
-        productoAActualizar.setProdmarca(txtMarca.getText());
-        productoAActualizar.setProdprecio(Integer.parseInt(txtPrecio.getText()));
-        productoDAO.update(productoAActualizar);
+        daoConcepto conceptoDAO = new daoConcepto();
+        clsConcepto conceptoAActualizar = new clsConcepto();
+        conceptoAActualizar.setConid(Integer.parseInt(txtbuscado.getText()));
+        conceptoAActualizar.setConnombre(txtConNombre.getText());
+        conceptoAActualizar.setConefecto(txtConEfecto.getText());
+        conceptoAActualizar.setConestatus(txtConEstado.getText());
+        conceptoDAO.update(conceptoAActualizar);
         llenadoDeTablas();
     }//GEN-LAST:event_btnModificarActionPerformed
 
     private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
        
-        txtProveedor.setText("");
-        txtPrecio.setText("");
+        txtConId.setText("");
+        txtConEstado.setText("");
         txtbuscado.setText("");
-        txtNombre.setText("");
-        txtMarca.setText("");
+        txtConNombre.setText("");
+        txtConEfecto.setText("");
         btnRegistrar.setEnabled(true);
         btnModificar.setEnabled(true);
         btnEliminar.setEnabled(true);
@@ -311,10 +307,10 @@ public class frmMantenimientoProducto extends javax.swing.JInternalFrame {
     private javax.swing.JLabel lb2;
     private javax.swing.JLabel lbusu;
     private javax.swing.JTable tablaVendedores;
-    private javax.swing.JTextField txtMarca;
-    private javax.swing.JTextField txtNombre;
-    private javax.swing.JTextField txtPrecio;
-    private javax.swing.JTextField txtProveedor;
+    private javax.swing.JTextField txtConEfecto;
+    private javax.swing.JTextField txtConEstado;
+    private javax.swing.JTextField txtConId;
+    private javax.swing.JTextField txtConNombre;
     private javax.swing.JTextField txtbuscado;
     // End of variables declaration//GEN-END:variables
 }
