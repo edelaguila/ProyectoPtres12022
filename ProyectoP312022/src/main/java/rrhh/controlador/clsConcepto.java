0613fe2a0c4e9b9fcfb9326d0a74465aa255bf3c
Hector Rosales <ronahect29@gmail.com>
2022-04-30 19:52:33 -0600

    Creacion del godigo de la clase Concepto

--- ProyectoP312022/src/main/java/rrhh/controlador/clsConcepto.java
+++ ProyectoP312022/src/main/java/rrhh/controlador/clsConcepto.java
@@ -5,56 +5,64 @@
  */
 package rrhh.controlador;
 
-import seguridad.controlador.*;
 
 /**
  *
  * @author visitante
  */
 public class clsConcepto {
-    int aplid;
-    String aplnombre;
-    String aplestatus;
+    private int Concepid;
+    private String Concepnombre;
+    private String Concepefecto; 
+    private String Concepestado;
+    
 
     public clsConcepto() {
     }
 
-    public clsConcepto(int id_aplicacion) {
-        this.aplid= id_aplicacion;
+    public clsConcepto(int id_Concepto) {
+        this.Concepid= id_Concepto;
     }
 
-    public clsConcepto(String nombreAplicacion, String estadoAplicacion) {
-        this.aplnombre = nombreAplicacion;
-        this.aplestatus = estadoAplicacion;
+    public clsConcepto(String nombre, String estado) {
+        this. Concepnombre = nombre;
+        this.Concepestado = estado;
     }
 
-    public int getId_aplicacion() {
-        return aplid;
+    public int getId_Concepto() {
+        return Concepid;
     }
 
-    public void setId_aplicacion(int id_aplicacion) {
-        this.aplid = id_aplicacion;
+    public void setId_Concepto(int id_Concepto) {
+        this.Concepid = id_Concepto;
     }
 
-    public String getNombreAplicacion() {
-        return aplnombre;
+    public String getNombre() {
+        return Concepnombre;
     }
 
-    public void setNombreAplicacion(String nombreAplicacion) {
-        this.aplnombre = nombreAplicacion;
+    public void setNombre(String nombre) {
+        this.Concepnombre = nombre;
     }
 
-    public String getestadoAplicacion() {
-        return aplestatus;
+    public String getestado() {
+        return Concepestado;
     }
 
-    public void setestadoAplicacion(String estadoAplicacion) {
-        this.aplestatus = estadoAplicacion;
+    public void setestado(String estado) {
+        this.Concepestado = estado;
     }
+    public String getefecto() {
+        return Concepefecto;
+    }
+     public void setefecto(String efecto) {
+        this.Concepefecto = efecto;
+     }
 
     @Override
     public String toString() {
-        return "tbl_aplicacion{" + "aplid=" + aplid + ", aplnombre=" + aplnombre + ", aplestatus=" + aplestatus + '}';
+        return "tbl_Concepto{" + "Concepid=" + Concepid + ", Concepnombre=" + Concepnombre + ", Concepestado=" + Concepestado + ",Concepefecto="+ Concepefecto+'}';
     }
     
-}
+     }
+
