d317835bcf4dd0e876a4c3e7f44f5e670307d2c7
Hector Rosales <ronahect29@gmail.com>
2022-04-30 19:47:52 -0600

    agregue codigo a la clase concepto

--- ProyectoP312022/src/main/java/rrhh/controlador/clsConcepto.java
+++ ProyectoP312022/src/main/java/rrhh/controlador/clsConcepto.java
@@ -5,12 +5,56 @@
  */
 package rrhh.controlador;
 
-import prototipos.controlador.*;
+import seguridad.controlador.*;
 
 /**
  *
- * @author DELL
+ * @author visitante
  */
 public class clsConcepto {
+    int aplid;
+    String aplnombre;
+    String aplestatus;
+
+    public clsConcepto() {
+    }
+
+    public clsConcepto(int id_aplicacion) {
+        this.aplid= id_aplicacion;
+    }
+
+    public clsConcepto(String nombreAplicacion, String estadoAplicacion) {
+        this.aplnombre = nombreAplicacion;
+        this.aplestatus = estadoAplicacion;
+    }
+
+    public int getId_aplicacion() {
+        return aplid;
+    }
+
+    public void setId_aplicacion(int id_aplicacion) {
+        this.aplid = id_aplicacion;
+    }
+
+    public String getNombreAplicacion() {
+        return aplnombre;
+    }
+
+    public void setNombreAplicacion(String nombreAplicacion) {
+        this.aplnombre = nombreAplicacion;
+    }
+
+    public String getestadoAplicacion() {
+        return aplestatus;
+    }
+
+    public void setestadoAplicacion(String estadoAplicacion) {
+        this.aplestatus = estadoAplicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "tbl_aplicacion{" + "aplid=" + aplid + ", aplnombre=" + aplnombre + ", aplestatus=" + aplestatus + '}';
+    }
     
 }
