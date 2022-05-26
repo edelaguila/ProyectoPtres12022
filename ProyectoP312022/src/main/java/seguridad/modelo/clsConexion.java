4582a4c1495414da15836a1ada259a3478e9d77d
DanielRoblero1 <98116526+DanielRoblero1@users.noreply.github.com>
2022-05-25 19:32:24 -0500

    arreglo conexcion

--- ProyectoP312022/src/main/java/seguridad/modelo/clsConexion.java
+++ ProyectoP312022/src/main/java/seguridad/modelo/clsConexion.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class clsConexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost/bdproyectop312022?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "12345";
+    private static final String JDBC_PASS = "123";
    
     
     public static Connection getConnection() throws SQLException {
